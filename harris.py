699d5c999ce56bfde02cb8c04bb03c77f658eb5a
Onkur Sen <onkursen@gmail.com>
2015-03-16 17:30:41 -0500

    separate matrix calcuation from score calculation

--- harris.py
+++ harris.py
@@ -9,11 +9,11 @@ import sys
 # http://opencv-python-tutroals.readthedocs.org/en/latest/py_tutorials/py_imgproc/py_gradients/py_gradients.html
 
 # Same argument set as cv2.cornerHarris
-def harris(src, blockSize, ksize, k):
+def generate_gradient_matrix(src, blockSize, ksize, k):
 	o = time()
 
 	# Return value of the method
-	dst = np.zeros(src.shape)
+	dst = {}
 
 	# Use depth constants based on this StackOverflow article:
 	# http://stackoverflow.com/q/11331830/
@@ -24,6 +24,7 @@ def harris(src, blockSize, ksize, k):
 	for y in xrange(size_y):
 		for x in xrange(size_x):
 			M = np.zeros((2, 2))
+
 			# Use a blockSize x blockSize window (except for pixels too close to the edges)
 			for v in xrange(max(0, y - blockSize / 2), min(size_y, y + blockSize / 2)):
 				for u in xrange(max(0, x - blockSize / 2), min(size_x, x + blockSize / 2)):
@@ -34,8 +35,19 @@ def harris(src, blockSize, ksize, k):
 					M[1, 1] += np.dot(gradient_y[v, u], gradient_y[v, u])
 
 			# Calculate score as given in the paper
-			dst[y, x] = np.linalg.det(M) - k * (np.trace(M) ** 2)
+			dst[(y, x)] = M
+			
 	print 'Total time: %.3f' % (time() - o)
+	return dst
+
+def harris(src, blockSize, ksize, k):
+	gradient_matrix = generate_gradient_matrix(src, blockSize, ksize, k)
+	dst = np.zeros(src.shape)
+	size_y, size_x = src.shape
+	for y in xrange(size_y):
+		for x in xrange(size_x):
+			M = gradient_matrix[(y, x)]
+			dst[(y, x)] = np.linalg.det(M) - k * (np.trace(M) ** 2)
 	return dst
 
 using_own_implementation = True
